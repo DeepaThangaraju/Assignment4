807ca51ad9d5c9c2f093851af9472f67e03e4f45
DeepaThangaraju <91173451+DeepaThangaraju@users.noreply.github.com>
2021-09-24 23:52:39 +0530

    Update isequal.js

--- isequal.js
+++ isequal.js
@@ -1,17 +1,12 @@
 var obj1 = { name: "Person 1", age:5 };
 var obj2 = { age:5, name: "Person 1" };
 var len=Object.keys(obj1).length;
-    var count=0;
- console.log(len)  ; 
+var count=0;
+console.log(len)  ; 
 for(var key in obj1){
     for(var key1 in obj2){
-        if(key==key1){
-            count++;
+        if(obj1[key]==obj2[key1]){
+            console.log("equal");
         }
     }
-}
-if(len===count){
-    console.log("equal");
-}else{
-    console.log("notequal");
 }
